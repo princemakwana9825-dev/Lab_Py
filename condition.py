850af092d0055352875512f52a8ddccbb17a94eb
princemakwana9825-dev <princemakwana9825@gmail.com>
2026-02-11 09:12:51 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,28 @@
+""" age=int(input("enter your age :"))
+if age >=18:
+    print("yes you are elijible")
+else:
+    print("you are not elijible") """ 
+    
+""" marks = int(input("emter your mark :"))
+if marks>= 90:
+    print("you get A grade")
+elif marks>=70:
+    print("you get B grade")
+else:
+    print("you get C grade") """ 
+
+""" num =int(input("enter a number :"))
+if num % 2==0:   
+    print("even number")
+else:
+    print("odd number") """ 
+    
+num = int(input("enter number :"))
+if num > 0:
+    print("positive number")
+elif num < 0:
+    print("negetive numder")
+else:
+    print("zero")    
+    
