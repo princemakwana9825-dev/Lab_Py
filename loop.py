adbf4a52fab1a914cbb000c42e0c6a7ec66beadb
princemakwana9825-dev <princemakwana9825@gmail.com>
2026-02-16 11:38:41 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,41 @@
+""" ex 1
+for i in range (1,6):
+    print(i) """
+    
+""" ex 2
+for i in range (3):
+    print("hello")"""
+    
+""" ex 3
+for i in range (1,11):
+    print(i)"""
+    
+""" ex 4 
+for i in range (1,21):
+ if i % 2==0:
+    print(i) """
+    
+""" ex 5 
+for i in range (1,16):
+    if i % 2!=0:
+        print(i) """ 
+        
+""" ex 6 
+for i in range (1,11):
+    print("5 x", i ,"=",5*i) """
+    
+""" ex 7 
+name="atmiya"
+
+for letter in name:
+    print(letter) """ 
+    
+""" ex 8 
+for i in range (1,6):
+    total=total + i :
+    print("sum is :",total) """
+    
+""" ex 9 
+numbers = [10,20,30,40]
+for n in numbers:
+    print(n) """
